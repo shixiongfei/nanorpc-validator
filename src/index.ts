80fef1eec1a0bbbca9734abea5fd9ed0b8f7a334
shixiongfei <xiongfei.shi@icloud.com>
2024-04-05 19:50:29 +0800

    Init project

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/*
+ * index.ts
+ *
+ * Copyright (c) 2024 Xiongfei Shi
+ *
+ * Author: Xiongfei Shi <xiongfei.shi(a)icloud.com>
+ * License: Apache-2.0
+ *
+ * https://github.com/shixiongfei/nanorpc-validator
+ */
